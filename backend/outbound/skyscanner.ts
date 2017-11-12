16afa209ac8e3d79a336eb5e505bba1609285487
Valentin Moullet <valentin@tsf-476-wpa-1-093.epfl.ch>
2017-11-12 04:24:44 +0100

    Fixed airports

--- backend/outbound/skyscanner.ts
+++ backend/outbound/skyscanner.ts
@@ -27,29 +27,34 @@ export class Skyscanner {
         let dayDeparture: string = departureDate.toISOString().slice(0, 10);
         let dayReturn: string = returnDate.toISOString().slice(0, 10);
 
+        console.log(dayDeparture);
+
         let cities: Array<string> = [];
         let promises: Array<Promise.Promise<[string, string]>> = [];
         for (let i = 0; i < users.length; i++) {
             cities[i] = users[i].departure;
             let query: string = url + '/browsequotes/v1.0/ch/chf/en-US/'+ cities[i] + '/'
                 + destCountry + '/' + departureDate + '/' + returnDate + '?apikey=' + apikey;
+            console.log(query);
             promises.push(Skyscanner.requestSuggestionAsync(query, users[i].name));
         }
 
+        /*
         let suggPromise: Promise.Promise<Array<Suggestion>> = Promise.Promise.all(promises).then((alldata) => {
-            console.log(alldata);
             let dataByUser: Array<[string, any]> = _.map(alldata, (x: [string, string]) => [x[0], JSON.parse(x[1])]);
+            console.log("tesss");
             let quotesByUser: Array<[string, Array<Quote>]> = _.map(dataByUser, (x: [string, any]) => [x[0], x[1].Quotes]);
-            let places: any = dataByUser.flatMap((data) => _.map(data[1].Places, (place: Place) => [place.PlaceId, place.Name]));
-            let airportByUser: Array<[string,Array<number>]> = _.map(quotesByUser, (x: [string,Array<number>]) => [x[0], _.map(x[1], (q: Quote) => q.OutboundLeg.DestinationId)]);
+            let places = dataByUser.flatMap((data) => _.map(data[1].Places, (place) => [place.PlaceId, place.Name]));
+            // console.log(Skyscanner.uniq(places));
+            let airportByUser: Array<[string,Array<number>]> = _.map(quotesByUser, (x) => [x[0], _.map(x[1], (q: Quote) => q.OutboundLeg.DestinationId)]);
             let airports = _.map(airportByUser, (x:any) => x[1])
             let validAirports = _.intersection(...airports);
 
             let useQuotesFlattened: Array<[string, Quote]> = quotesByUser.flatMap<[string, Quote]>(
                 (uQuotes) => _.map(uQuotes[1], (quote: Quote) => [uQuotes[0], quote]));
             let destToQuote: any = _.groupBy(useQuotesFlattened, (x: [string, Quote]) => x[1].OutboundLeg.DestinationId);
-            let s: any = _.pick(destToQuote, validAirports);
-            let res: Array<[string, Quote]> = [];
+            let s:Array<number, Quote> = _.pick(destToQuote, validAirports);
+            let res = []
             for(let key in s) {
                 for(let i = 0; i < places.length; i ++) {
                     if (key == places[i][0]) {
@@ -59,17 +64,19 @@ export class Skyscanner {
                 }
             }
 
-            let rr: Array<Suggestion> = _.map(res, (r: any) => 
-                new Suggestion(r[0], _.map(r[1], (uq: any) => new UserWithSuggestion(uq[0], uq[1].MinPrice))));
+            let rr = _.map(res, (r) =>{  return new Suggestion(r[0], _.map(r[1], (uq)=> new UserWithSuggestion(uq[0],uq[1].MinPrice))});
+            console.log(util.inspect(res, false, null));
+            console.log(util.inspect(rr, false, null));
 
             return rr;
         });
+        */
 
-        return suggPromise;
+        return Promise.Promise.resolve([]);
     }
 
-    static getAirports(): string {
-        return fs.readFileSync('./data/airports.json')
+    static getAirports(): any {
+        return {"data": fs.readFileSync('./data/airports.json', 'utf8')};
     }
 
     private static requestSuggestionAsync(query: string, user: string): Promise.Promise<[string, string]> {
